0109fafbf80871a2b6714713a4a36e83a0a414fe
lordirah <lordirah@gmail.com>
2022-12-05 19:02:29 +0530

    day 5 solutions

---
+++ day05/part1.py
@@ -0,0 +1,80 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> str:
+    lines = s.splitlines()
+    data_dict = dict()
+    final = ''
+    for line in lines:
+        if line.startswith(' 1'):
+            keys_pos = list(line)
+    for pos, key in enumerate(keys_pos):
+        if key != ' ':
+            data_dict[key] = []
+            for line in lines:
+                if line.startswith(' 1'):
+                    break
+                if list(line)[pos] != ' ':
+                    data_dict[key].insert(0, list(line)[pos])
+    for i, line in enumerate(lines):
+        if line == '':
+            empty_line = i
+    for i, line in enumerate(lines):
+        if i > empty_line:
+            row = line.split(' ')
+            no_items = row[1]
+            key = row[3]
+            to_key = row[5]
+            for i in range(int(no_items)):
+                data_dict[to_key].append(data_dict[key].pop(-1))
+    for k, v in data_dict.items():
+        final = final + v[-1]
+    return final
+
+
+INPUT_S = '''\
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+'''
+EXPECTED = 'CMZ'
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
